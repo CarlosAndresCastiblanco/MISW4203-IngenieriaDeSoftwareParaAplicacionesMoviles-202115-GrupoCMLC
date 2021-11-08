0c6c0f9559a8dc7d5bd778b53160555e007f7337
Carlos Andrés Castiblanco Castro <c.castiblancoc@uniandes.edu.co>
2021-11-07 23:52:21 -0500

    :construction: CC feature/refactorJetpack

--- app/src/androidTest/java/com/uniandes/vinilos/ui/activity/AlbumListTest.kt
+++ app/src/androidTest/java/com/uniandes/vinilos/ui/activity/AlbumListTest.kt
@@ -1,49 +1,89 @@
 package com.uniandes.vinilos.ui.activity
-
-import android.media.JetPlayer
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.testing.TestNavHostController
-import androidx.test.core.app.ApplicationProvider
-import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.uniandes.vinilos.HomeScreen
+import com.uniandes.vinilos.DetailsScreen
+
 import com.uniandes.vinilos.ListScreen
 import com.uniandes.vinilos.MainActivity
+import com.uniandes.vinilos.model.Performer
+import com.uniandes.vinilos.ui.theme.AlbumsAppTheme
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+
 import org.junit.Before
-
-import org.junit.Test
-import org.junit.runner.RunWith
-
 import org.junit.Rule
+import org.junit.Test
 
-@RunWith(AndroidJUnit4::class)
+@HiltAndroidTest
 class AlbumListTest {
-    @get:Rule var activityScenarioRule = createAndroidComposeRule<MainActivity>()
-    private lateinit var navController : TestNavHostController
-    @get:Rule val composeTestRule = createComposeRule()
 
+    @get:Rule(order = 0)
+    val hiltRule = HiltAndroidRule(this)
+
+    @get:Rule(order = 1)
+    val composeRule = createAndroidComposeRule<MainActivity>()
+
+    @ExperimentalAnimationApi
     @Before
     fun setUp() {
-        navController = TestNavHostController(ApplicationProvider.getApplicationContext())
+        hiltRule.inject()
+        composeRule.setContent {
+            val navController = rememberNavController()
+            AlbumsAppTheme() {
+                NavHost(
+                    navController = navController,
+                    startDestination = "home"
+                ) {
+                    composable(route = "home") {
+                        ListScreen(navController = navController)
+                    }
+                    composable(route = "DETAILS_SCREEN/1") {
+                        DetailsScreen("1", navController = navController)
+                    }
+                }
+            }
+        }
     }
 
     @Test
-    fun checkActivityVisibility(){
-        activityScenarioRule.onNodeWithText("Lista de Álbumes").assertExists()
+    fun validateListAlbumActivity() {
+        Thread.sleep(500)
+        composeRule.onNodeWithText("Lista de Álbumes").assertExists()
+        Thread.sleep(500)
+        composeRule.onNodeWithText("Album 1").assertExists()
+        composeRule.onNodeWithText("Artista 1").assertExists()
+        Thread.sleep(500)
+        composeRule.onNodeWithText("Album 2").assertExists()
+        composeRule.onNodeWithText("Artista 2").assertExists()
     }
 
     @Test
-    fun selectAnyAlbumList(){
-        composeTestRule.setContent { HomeScreen() }
-        Thread.sleep(3000)
-        composeTestRule.onNodeWithText("Buscando América").assertExists()
+    fun validateDetailAlbumActivity() {
+        Thread.sleep(500)
+        composeRule.onNodeWithText("Album 1").assertExists()
+        //On click details
+        composeRule.onNodeWithText("Album 1").performClick()
+        Thread.sleep(2000)
+        //Artista
+        composeRule.onNodeWithText("Artista 1").assertExists()
+        //Salsa
+        composeRule.onNodeWithText("Salsa").assertExists()
+        //Record label
+        composeRule.onNodeWithText("EMI").assertExists()
+        //Release Date
+        composeRule.onNodeWithText("1948-07-16").assertExists()
 
-        //activityScenarioRule.onNodeWithTag("albumItem").performClick()
-        //activityScenarioRule.onNodeWithText("Artista").assertExists()
-        //activityScenarioRule.onNodeWithText("Bellido de Luna").assertExists()
+
+        //Validate return button OnClick
+        composeRule.onNodeWithContentDescription("Back").performClick()
+        // Validate Album List
+        Thread.sleep(2000)
+        composeRule.onNodeWithText("Album 1").assertExists()
+        composeRule.onNodeWithText("Album 2").assertExists()
+        Thread.sleep(1000)
     }
 }
