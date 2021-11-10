e75717f70d5131ea43b3f120af0f69906f4c87fa
Luis Castillo <l.castillo1@uniandes.edu.co>
2021-11-09 21:53:05 -0500

    Add call method for request collector details in repository and provider

--- app/src/main/java/com/uniandes/vinilos/provider/CollectorsProvider.kt
+++ app/src/main/java/com/uniandes/vinilos/provider/CollectorsProvider.kt
@@ -8,4 +8,7 @@ import retrofit2.http.Path
 interface CollectorsProvider {
     @GET("collectors")
     suspend fun getCollectors(): Response<List<Collector>>
+
+    @GET("collectors/{id}")
+    suspend fun getCollectorDetail(@Path("id") country: String): Response<Collector>
 }
--- app/src/main/java/com/uniandes/vinilos/repository/CollectorsRepository.kt
+++ app/src/main/java/com/uniandes/vinilos/repository/CollectorsRepository.kt
@@ -1,11 +1,13 @@
 package com.uniandes.vinilos.repository
 
 import com.uniandes.vinilos.model.*
+import com.uniandes.vinilos.provider.AlbumsProvider
 import com.uniandes.vinilos.provider.CollectorsProvider
 import javax.inject.Inject
 
 interface CollectorsRepository {
     suspend fun getCollectors(): List<Collector>
+    suspend fun getCollector(id: String): Collector
 }
 
 class CollectorsRepositoryImp @Inject constructor(
@@ -13,12 +15,27 @@ class CollectorsRepositoryImp @Inject constructor(
 ): CollectorsRepository {
 
     private var collectors: List<Collector> = emptyList()
+    private var collector: Collector? = null
 
     override suspend fun getCollectors(): List<Collector> {
         val apiResponse = collectorsProvider.getCollectors().body()
 
         collectors = apiResponse ?: emptyList()
         return collectors
+    }
+
+    override suspend fun getCollector(id: String): Collector {
+        val apiResponse = collectorsProvider.getCollectorDetail(id).body()
+
+        var collectorPerfomers = arrayListOf(
+            CollectorPerformers("1", "Artista", "http:asd.com")
+        )
+        var collectorAlbums = arrayListOf(
+            CollectorAlbums("1", "590", "Active")
+        )
+        collector = apiResponse ?: Collector("1", "Performer", "1234567", "performer@music.com",
+            collectorPerfomers, collectorAlbums);
+        return collector as Collector
     }
 
 }
