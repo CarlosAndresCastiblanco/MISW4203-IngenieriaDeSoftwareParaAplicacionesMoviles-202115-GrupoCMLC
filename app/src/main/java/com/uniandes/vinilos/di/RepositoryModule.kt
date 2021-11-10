b9bfda8e2c105c1cfffe8df305f23eae1f528d96
Luis Castillo <l.castillo1@uniandes.edu.co>
2021-11-09 20:52:58 -0500

    Add dataintegration in providers and repositories

--- app/src/main/java/com/uniandes/vinilos/di/RepositoryModule.kt
+++ app/src/main/java/com/uniandes/vinilos/di/RepositoryModule.kt
@@ -1,8 +1,11 @@
 package com.uniandes.vinilos.di
 
 import com.uniandes.vinilos.provider.AlbumsProvider
+import com.uniandes.vinilos.provider.CollectorsProvider
 import com.uniandes.vinilos.repository.AlbumsRepository
 import com.uniandes.vinilos.repository.AlbumsRepositoryImp
+import com.uniandes.vinilos.repository.CollectorsRepository
+import com.uniandes.vinilos.repository.CollectorsRepositoryImp
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -17,6 +20,11 @@ class RepositoryModule {
     @Singleton
     fun providerAlbumsRepository(provider: AlbumsProvider): AlbumsRepository =
         AlbumsRepositoryImp(provider)
+
+    @Provides
+    @Singleton
+    fun providerCollectorsRepository(provider: CollectorsProvider): CollectorsRepository =
+        CollectorsRepositoryImp(provider)
 }
 
 
